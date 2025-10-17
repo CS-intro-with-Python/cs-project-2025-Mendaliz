6ba0e93657a773c403248df1e271869b7eda8cb7
Dmitrii Desiatov <dimokkord@gmail.com>
2025-10-17 18:27:46 +0300

    Changes for session 2 task 2

--- first_project/client.py
+++ first_project/client.py
@@ -1,10 +1,22 @@
 import requests
-import json
 
-def make_request(port=8080):
+def make_request_hello(port=8080):
     response = requests.get(f"http://127.0.0.1:{port}/hello")
     print(response)
     print(response.json())
 
+def make_response_user(port=8080, name="Tehia"):
+    response = requests.get(f"http://127.0.0.1:{port}/user/{name}")
+    print(response)
+    print(response.json())
+
+def make_response_search(port=8080, query="Hoot"):
+    response = requests.get(f"http://127.0.0.1:{port}/search?q={query}")
+    print(response)
+    print(response.json())
+
 if __name__ == '__main__':
-    make_request(port=8080)
+    port = 8080
+    make_request_hello(port)
+    make_response_user(port)
+    make_response_search(port)
